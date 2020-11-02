a69d5e19cd0998d275b6649934e1669258ec2eaf
jaywalkingstreetparking <jjyyrim@gmail.com>
2020-11-01 21:50:23 -0800

    Register text now leads to a registration page successfully

--- app/src/main/java/ca/bcit/groupproject/SignUpActivity.java
+++ app/src/main/java/ca/bcit/groupproject/SignUpActivity.java
@@ -1,6 +1,13 @@
 package ca.bcit.groupproject;
 
 import android.app.Activity;
+import android.os.Bundle;
 
 public class SignUpActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+    }
 }
