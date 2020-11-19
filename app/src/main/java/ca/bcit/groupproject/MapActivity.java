0b8489aaa9189d2aef909a7a44f3a93b994dd5c7
jaywalkingstreetparking <jjyyrim@gmail.com>
2020-11-18 23:23:17 -0800

    Map activity to map layout

--- app/src/main/java/ca/bcit/groupproject/MapActivity.java
+++ app/src/main/java/ca/bcit/groupproject/MapActivity.java
@@ -1,6 +1,13 @@
 package ca.bcit.groupproject;
 
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MapActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map);
+    }
 }
