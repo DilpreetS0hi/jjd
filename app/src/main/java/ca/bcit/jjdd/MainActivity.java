0c995b17c1bfc64dffd50f22a60c47fcfe4654fb
DilpreetSohi <DilpreetSohi@.github.com>
2020-11-25 00:12:27 -0800

    origin

---
+++ app/src/main/java/ca/bcit/jjdd/MainActivity.java
@@ -0,0 +1,28 @@
+package ca.bcit.jjdd;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.GeolocationPermissions;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends Activity {
+    WebView mWebView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mWebView = findViewById(R.id.webView);
+        mWebView.getSettings().setAppCacheEnabled(true);
+        mWebView.getSettings().setDatabaseEnabled(true);
+        mWebView.getSettings().setDomStorageEnabled(true);
+        mWebView.getSettings().setJavaScriptCanOpenWindowsAutomatically(true);
+        mWebView.getSettings().setBuiltInZoomControls(true);
+        mWebView.getSettings().setJavaScriptEnabled(true);
+        mWebView.loadUrl("https://storage.googleapis.com/dilpreet-team2/index.html");
+    }
+}
